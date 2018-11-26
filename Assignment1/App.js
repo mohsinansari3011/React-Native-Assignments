30ade95019159be1c35549a7b363efbd6e1e4e20
khurramAkram30 <khurram.akram43@gmail.com>
2018-11-26 20:19:49 +0500

    Assignment1

---
+++ Assignment1/App.js
@@ -0,0 +1,138 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { FaceDetector,Camera,Permissions,Constants } from 'expo';
+
+export default class App extends React.Component {
+  state = {
+      hasCameraPermission:null,
+      faces : [],
+      pagechange:false
+    }
+    async componentWillMount() {
+       const { status } =await Permissions.askAsync(Permissions.CAMERA);
+       this.setState({hasCameraPermission:status==='granted'});
+    }
+
+    renderFaces = () => 
+    <View style={styles.facesContainer} pointerEvents="none">
+      {this.state.faces.map(this.renderFace)}
+    </View>
+
+    renderFace({ bounds, faceID, rollAngle, yawAngle }) {
+    return (
+      <View
+        key={faceID}
+        transform={[
+          { perspective: 600 },
+          { rotateZ: `${rollAngle.toFixed(0)}deg` },
+          { rotateY: `${yawAngle.toFixed(0)}deg` },
+        ]}
+        style={[
+          styles.face,
+          {
+            ...bounds.size,
+            left: bounds.origin.x,
+            top: bounds.origin.y,
+          },
+        ]}>
+        <Text style={styles.faceText}>rollAngle: {rollAngle.toFixed(0)}</Text>
+        <Text style={styles.faceText}>yawAngle: {yawAngle.toFixed(0)}</Text>
+      </View>
+    );
+  }
+
+
+  render() {
+    const { hasCameraPermission } = this.state;
+
+    if (hasCameraPermission === null) {
+      return <Text>Requesting for camera permission</Text>;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={styles.container}>
+          <Camera
+             style={styles.camera}
+             type={'front'}
+             onFacesDetected={this.handleFacesDetected}
+             faceDetectorSettings={{
+               mode: FaceDetector.Constants.Mode.fast,
+               detectLandmarks: FaceDetector.Constants.Mode.none,
+               runClassifications: FaceDetector.Constants.Mode.none,
+             }}>
+              <View style={styles.topBar}>
+                <Text style={styles.textcolor}>x: {this.state.faces.length ? this.state.faces[0].bounds.origin.x.toFixed(0) : 0}</Text>
+                <Text style={styles.textcolor}>y: {this.state.faces.length ? this.state.faces[0].bounds.origin.y.toFixed(0) : 0}</Text>
+              </View>
+              <View style={styles.bottomBar}>
+                <Text style={styles.textcolor}>Heigth: {this.state.faces.length ? this.state.faces[0].bounds.size.height.toFixed(0) : 0}</Text>
+                <Text style={styles.textcolor}>width: {this.state.faces.length ? this.state.faces[0].bounds.size.width.toFixed(0) : 0}</Text>
+              </View>
+             </Camera>
+             { this.state.faces.length ? this.renderFaces() : undefined}
+             {pagechange && <Appnavigation/>}
+           
+        </View>
+      );
+    }
+  }
+
+  handleFacesDetected = ({ faces }) => {
+    const {pagechange} = this.state;
+    if(faces.length>0){
+      console.log("detected");
+      this.setState({ faces , pagechange:true });
+    }
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+  },
+  camera: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  topBar: {
+    flex: 0.2,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingTop: Constants.statusBarHeight+1,
+  },
+  bottomBar: {
+    flex: 0.2,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  face: {
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 1,
+    position: 'absolute',
+    borderColor: '#808000',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+  },
+  faceText: {
+    color: '#32CD32',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: 10,
+    backgroundColor: 'transparent',
+  },
+  facesContainer: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    left: 0,
+    top: 0,
+  },
+  textcolor:{
+    color: '#008080',
+  }
+});
---
+++ Assignment1/screens/HomeScreen.js
@@ -0,0 +1,225 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { WebBrowser } from 'expo';
+
+import { MonoText } from '../components/StyledText';
+
+export default class HomeScreen extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      
+    }
+  }
+  static navigationOptions = {
+    header: null,
+  };
+
+  // componentDidMount(){
+  //   this.buildquiz();
+  // }
+  buildquiz(){
+    fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy').then(function(res){
+      return res.json()
+    }).then(function(res){
+      console.log(res.results)
+      // image.remove();
+      for(var i = 0; i < res.results.length; i++){
+        var paragraph = document.createElement('p');
+        quizForm.innerHTML+=`${score+1} ${res.results[i].question} </br>`;
+        // paragraph.innerHTML = res.results[i].question;
+        var incorrectanswers=res.results[i].incorrect_answers;
+        // console.log("test",res.results[i].incorrect_answers);
+        for(var a=0;a<3;a++){
+          console.log(res.results[i].incorrect_answers[a]);
+         
+            
+            quizForm.innerHTML+=`<input type ="radio" value="${res.results[i].incorrect_answers[a]}"> ${res.results[i].incorrect_answers[a]}</br>`;
+            
+          }
+          quizForm.innerHTML+=`<input type ="radio" value="${res.results[i].correct_answer}"> ${res.results[i].correct_answer}</br>`;
+      score++;
+      }
+    })
+    }
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+          <View style={styles.welcomeContainer}>
+            <Image
+              source={
+                __DEV__
+                  ? require('../assets/images/robot-dev.png')
+                  : require('../assets/images/robot-prod.png')
+              }
+              style={styles.welcomeImage}
+            />
+          </View>
+
+          <View style={styles.getStartedContainer}>
+            {this._maybeRenderDevelopmentModeWarning()}
+
+            <Text style={styles.getStartedText}>Get started by opening</Text>
+
+            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
+              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
+            </View>
+
+            <Text style={styles.getStartedText}>
+              Change this text and your app will automatically reload.
+            </Text>
+          </View>
+
+          <View style={styles.helpContainer}>
+            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
+              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+
+        <View style={styles.tabBarInfoContainer}>
+          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
+
+          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
+            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
+          </View>
+        </View>
+      </View>
+    );
+  }
+
+  _maybeRenderDevelopmentModeWarning() {
+    if (__DEV__) {
+     
+      const learnMoreButton = (
+        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
+          khurram
+          
+        </Text>
+      );
+      this.buildquiz();
+
+      return (
+        <Text style={styles.developmentModeText}>
+          . {learnMoreButton}
+        </Text>
+      );
+    } else {
+      return (
+        <Text style={styles.developmentModeText}>
+          You are not in development mode, your app will run at full speed.
+        </Text>
+      );
+    }
+  }
+
+  _handleLearnMorePress = () => {
+    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
+  };
+
+  _handleHelpPress = () => {
+    WebBrowser.openBrowserAsync(
+      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
+    );
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  developmentModeText: {
+    marginBottom: 20,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: 'center',
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  homeScreenFilename: {
+    marginVertical: 7,
+  },
+  codeHighlightText: {
+    color: 'rgba(96,100,109, 0.8)',
+  },
+  codeHighlightContainer: {
+    backgroundColor: 'rgba(0,0,0,0.05)',
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  tabBarInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: 'center',
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 20,
+  },
+  tabBarInfoText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    textAlign: 'center',
+  },
+  navigationFilename: {
+    marginTop: 5,
+  },
+  helpContainer: {
+    marginTop: 15,
+    alignItems: 'center',
+  },
+  helpLink: {
+    paddingVertical: 15,
+  },
+  helpLinkText: {
+    fontSize: 14,
+    color: '#2e78b7',
+  },
+});
